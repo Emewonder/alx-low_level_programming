2303725ddab86fc70994ae6276a78557525a8d24
Emewonder <emewonder@gmail.com>
2022-09-12 16:48:21 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	while (d < 58)
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}
