e52c2de97099b0e57c660ca9e4fb1de19d609fa0
Emewonder <emewonder@gmail.com>
2022-09-14 17:14:28 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - Prints all natural numbers from input to 98,
@@ -12,13 +12,13 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 			printf("%d, ", n--);
-		 Printf("%d\n", n);
+		 printf("%d\n", n);
 	}
 
 	else
 	{
 		while (n < 98)
-			Printf("%d, ", n++);
-		Printf("%d\n", n);
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
