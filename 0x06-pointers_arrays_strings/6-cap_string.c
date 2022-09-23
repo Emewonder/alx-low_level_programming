176cba3225bd510a85cea5c6374721d0f859f0f0
Emewonder <emewonder@gmail.com>
2022-09-22 22:34:19 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
-* cap_string - Capitalizes all words of a string.
-* @str: The string to be capitalized.
-*
-* Return: A pointer to the changed string.
-*/
-
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
 char *cap_string(char *str)
 {
-
 	int index = 0;
 
 	while (str[index])
@@ -32,7 +30,9 @@ char *cap_string(char *str)
 		    str[index - 1] == '}' ||
 		    index == 0)
 			str[index] -= 32;
+
 		index++;
 	}
+
 	return (str);
 }
